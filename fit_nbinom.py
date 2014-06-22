ca88fb9434ec09b18fbde77ed208d5f114ec24c0
Gökçen Eraslan <gokcen.eraslan@gmail.com>
2014-06-22 12:29:39 +0300

    Use infinitesimal value provided by numpy.

--- fit_nbinom.py
+++ fit_nbinom.py
@@ -31,6 +31,8 @@ import sys
 # initial params is a numpy array representing the initial values of
 # size and prob parameters
 def fit_nbinom(X, initial_params=None):
+    infinitesimal = np.finfo(np.float).eps
+
     def log_likelihood(params, *args):
         r, p = params
         X = args[0]
@@ -42,7 +44,7 @@ def fit_nbinom(X, initial_params=None):
             - np.sum(np.log(factorial(X))) \
             - N*(gammaln(r)) \
             + N*r*np.log(p) \
-            + np.sum(X*np.log(1-(p if p < 1 else 1-1e-10)))
+            + np.sum(X*np.log(1-(p if p < 1 else 1-infinitesimal)))
 
         return -result
 
@@ -51,7 +53,7 @@ def fit_nbinom(X, initial_params=None):
         X = args[0]
         N = X.size
 
-        pderiv = (N*r)/p - np.sum(X)/(1-(p if p < 1 else 1-1e-10))
+        pderiv = (N*r)/p - np.sum(X)/(1-(p if p < 1 else 1-infinitesimal))
         rderiv = np.sum(psi(X + r)) \
             - N*psi(r) \
             + N*np.log(p)
@@ -59,17 +61,17 @@ def fit_nbinom(X, initial_params=None):
         return np.array([-rderiv, -pderiv])
 
     if initial_params is None:
-        #reasonable initial values from fitdistr function in R
+        #reasonable initial values (from fitdistr function in R)
         m = np.mean(X)
         v = np.var(X)
         size = (m**2)/(v-m) if v > m else 10
 
         #convert mu/size parameterization to prob/size
-        p0 = size / (size+m)
+        p0 = size / ((size+m) if size+m != 0 else 1)
         r0 = size
         initial_params = np.array([r0, p0])
 
-    bounds = [(1e-10, None), (1e-10, 1)]
+    bounds = [(infinitesimal, None), (infinitesimal, 1)]
     optimres = optim(log_likelihood,
                      x0=initial_params,
                      #fprime=log_likelihood_deriv,
