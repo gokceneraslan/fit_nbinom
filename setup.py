acda8e4d468d74fa1418c2406b2cd9a0f98834d4
塘由惟 <yuitomo@tsutsumiyuomonminoMacBook-puro.local>
2019-08-30 14:14:13 +0900

    :+1: create setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import os
+
+from setuptools import setup
+
+
+def read_requirements():
+    """Parse requirements from requirements.txt."""
+    reqs_path = os.path.join('.', 'requirements.txt')
+    with open(reqs_path, 'r') as f:
+        requirements = [line.rstrip() for line in f]
+    return requirements
+
+
+setup(
+    name="fit_nbinom",
+    version="0.0.1",
+    author="Gökçen Eraslan, Yui Tomo",
+    packages=["fit_nbinom"],
+    url="https://github.com/t-yui/fit_nbinom",
+    license="GNU General Public License",
+    description="Negative binomial maximum likelihood estimate implementation in Python using L-BFGS-B",
+    long_description=open('README.md').read(),
+    install_requires=read_requirements()
+)
