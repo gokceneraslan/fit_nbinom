152b8a8a22d4463804905b2d3921970241b3f7e4
Davide Cittaro <daweonline@gmail.com>
2022-01-27 12:58:29 +0100

    Fix import error
    `scipy.misc.factorial` has been deprecated, now is `scipy.special.factorial`

--- fit_nbinom.py
+++ fit_nbinom.py
@@ -21,7 +21,7 @@ from __future__ import print_function
 import numpy as np
 from scipy.special import gammaln
 from scipy.special import psi
-from scipy.misc import factorial
+from scipy.special import factorial
 from scipy.optimize import fmin_l_bfgs_b as optim
 
 import sys
